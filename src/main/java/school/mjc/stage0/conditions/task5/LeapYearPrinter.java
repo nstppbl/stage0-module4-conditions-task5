fbd54994d5873d318149a9da970d00165765e9b0
nstppbl <1nstppbl@gmail.com>
2022-09-02 18:01:53 +0500

    initial commit

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+
+        int remainder=((year%4==0)&&((year%400==0)||(year%100!=0))?1:0);
+
+        switch(remainder) {
+            case 1:
+                System.out.println("Leap Year");
+                break;
+
+            case 0:
+                System.out.println("Not Leap Year");
+                break;
+        }
+
     }
 }
